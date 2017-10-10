4f7a2d137c0c87372907c7064ee7df1437368214
Ryan Kubiak <rkubiak@google.com>
2017-10-10 12:49:57 -0700

    Testing stuff

--- bin/release.sh
+++ bin/release.sh
@@ -7,30 +7,43 @@ set -x
 
 function usage() {
   echo "$0 \
-    -t <tag name to apply to artifacts>"
+    -t <tag name to apply to artifacts> \
+    -v <version to apply instead of tag>"
   exit 1
 }
 
 # Initialize variables
 TAG_NAME=""
+VERSION_OVERRIDE=""
 
 # Handle command line args
-while getopts i:t: arg ; do
+while getopts i:t:v: arg ; do
   case "${arg}" in
     t) TAG_NAME="${OPTARG}";;
+    v) VERSION_OVERRIDE="${OPTARG}";;
     *) usage;;
   esac
 done
 
-mkdir -p $HOME/.docker
-gsutil cp gs://istio-secrets/dockerhub_config.json.enc $HOME/.docker/config.json.enc
-gcloud kms decrypt \
-       --ciphertext-file=$HOME/.docker/config.json.enc \
-       --plaintext-file=$HOME/.docker/config.json \
-       --location=global \
-       --keyring=Secrets \
-       --key=DockerHub
+if [ ! -z "${VERSION_OVERRIDE}" ] ; then
+  version="${VERSION_OVERRIDE}"
+elif [ ! -z "${TAG_NAME}" ] ; then
+  version="${TAG_NAME}"
+else
+  echo "Either -t or -v is a required argument."
+  usage
+fi
+
+#mkdir -p $HOME/.docker
+#gsutil cp gs://istio-secrets/dockerhub_config.json.enc $HOME/.docker/config.json.enc
+#gcloud kms decrypt \
+#       --ciphertext-file=$HOME/.docker/config.json.enc \
+#       --plaintext-file=$HOME/.docker/config.json \
+#       --location=global \
+#       --keyring=Secrets \
+#       --key=DockerHub
 
 ./bin/publish-docker-images.sh \
-    -h gcr.io/istio-io,docker.io/istio \
-    -t $TAG_NAME
+#    -h gcr.io/istio-io,docker.io/istio \
+    -h gcr.io/istio-builder-prototype \
+    -t $version
